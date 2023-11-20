8807e44c44c73140b0412ec941bc66b94fbdce55
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-11-20 16:43:54 +0900

    14. aタグとLinkコンポーネントの違い

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ use Inertia\Inertia;
 |
 */
 
+Route::get('/inertia-test', function () {
+    return Inertia::render('InertiaTest');
+});
+
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -35,4 +39,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
