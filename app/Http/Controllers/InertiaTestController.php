61c1ee46a6b75745eb486588b362e302d897f848
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-11-23 12:00:17 +0900

    20. Link ルートパラメータ

--- app/Http/Controllers/InertiaTestController.php
+++ app/Http/Controllers/InertiaTestController.php
@@ -11,4 +11,10 @@ class InertiaTestController extends Controller
     {
         return Inertia::render('Inertia/Index');
     }
+
+    public function show($id)
+    {
+        // dd($id);
+        return Inertia::render('Inertia/Show', ['id' => $id]);
+    }
 }
---
+++ resources/js/Pages/Inertia/Show.vue
@@ -0,0 +1,9 @@
+<script setup>
+defineProps({ // idの受け取り
+    id: String,
+})
+</script>
+
+<template>
+    {{ id }}
+</template>
