7f1474083edfcb25b843f15e8de14c7802607e61
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-12-01 20:09:21 +0900

    31. イベントコールバック(onBefore) その2

--- resources/js/Pages/Inertia/Show.vue
+++ resources/js/Pages/Inertia/Show.vue
@@ -1,12 +1,23 @@
 <script setup>
+import { Inertia } from '@inertiajs/inertia';
+
 defineProps({ // idの受け取り
     id: String,
     blog: Object, // 一件のみ取得の場合はObjectにする
 })
+
+const deleteConfirm = id => {
+    // console.log(id)
+    Inertia.delete(`/inertia/${id}`, {
+        onBefore: () => confirm('本当に削除しますか？')
+    })
+}
 </script>
 
 <template>
     {{ id }}
     <br>
     {{ blog.title }}
+    <br>
+    <button @click="deleteConfirm(blog.id)">削除</button>
 </template>
