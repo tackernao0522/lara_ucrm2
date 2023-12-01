3c60fe9c9e218327ce2cda230b3986c67e1b3c05
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-12-01 19:36:47 +0900

    30. イベントコールバック(onBefore) その1

--- resources/js/Pages/Inertia/Show.vue
+++ resources/js/Pages/Inertia/Show.vue
@@ -1,9 +1,12 @@
 <script setup>
 defineProps({ // idの受け取り
     id: String,
+    blog: Object, // 一件のみ取得の場合はObjectにする
 })
 </script>
 
 <template>
     {{ id }}
+    <br>
+    {{ blog.title }}
 </template>
