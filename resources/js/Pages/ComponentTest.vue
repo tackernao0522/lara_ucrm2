a97c4daea34ce0e93e723bc893b15ab297a9aba8
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-12-07 16:43:01 +0900

    33. 他のコンポーネントを使ってみる

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,12 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+import Label from '@/Components/InputLabel.vue'
+import Input from '@/Components/TextInput.vue'
+</script>
+
+<template>
+    <GuestLayout>
+        <Label value="件名">タイトル</Label>
+        <Input modelValue="初期値が入ります" /> <!--- このTextInput.vueへmodelValueを渡している -->
+    </GuestLayout>
+</template>
