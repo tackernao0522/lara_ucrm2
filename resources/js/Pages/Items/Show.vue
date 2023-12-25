0d4e6982086757918072e464415bfce0baa32c3a
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2023-12-25 16:50:11 +0900

    52. JSで bl2br 改行対応

--- resources/js/Pages/Items/Show.vue
+++ resources/js/Pages/Items/Show.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import nl2br from '@/common'
 import { Head } from '@inertiajs/vue3';
 
 defineProps({
@@ -37,9 +38,8 @@ defineProps({
                                         <div class="p-2 w-full">
                                             <div class="relative">
                                                 <label for="memo" class="leading-7 text-sm text-gray-600">メモ</label>
-                                                <div id="memo"
+                                                <div id="memo" v-html="nl2br(item.memo)"
                                                     class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">
-                                                    {{ item.memo }}
                                                 </div>
                                             </div>
                                         </div>
