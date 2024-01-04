2cbdaa0004db525e5afd45763924b9da5443a07a
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2024-01-04 17:14:54 +0900

    57. Customers 1000件ダミー登録

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Customer;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -24,5 +26,7 @@ class DatabaseSeeder extends Seeder
             UsersTableSeeder::class,
             ItemsTableSeeder::class,
         ]);
+
+        Customer::factory(1000)->create();
     }
 }
