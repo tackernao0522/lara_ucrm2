6db704714f17b14933e4f614ded5882f2e7ac91b
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2024-01-04 18:50:32 +0900

    61. ページネーション コンポーネントの作成

--- resources/js/Pages/Customers/Index.vue
+++ resources/js/Pages/Customers/Index.vue
@@ -2,16 +2,17 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import FlashMessage from '../../Components/FlashMessae.vue'
 import { Head, Link } from '@inertiajs/vue3';
-import { onMounted } from 'vue';
+import Pagination from '@/Components/Pagination.vue'
 
 const props = defineProps({
     customers: Object,
 })
-
-onMounted(() => {
-    console.log(props.customers)
-    console.log(props.customers.last_page)
-})
 </script>
 
-<template></template>
+<template>
+    <ul v-for="customer in customers.data" :key="customer.id">
+        <li>{{ customer.id }}</li>
+        <li>{{ customer.name }}</li>
+    </ul>
+    <Pagination class="mt-6" :links="customers.links"></Pagination>
+</template>
