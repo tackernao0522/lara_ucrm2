ab022d5ab795138b5253017bf1837fc4e9a47719
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2024-01-18 13:33:56 +0900

    73. 顧客情報をv-forで表示 74. 商品情報をv-forで表示 75. 合計金額をcomputedで計算

--- resources/js/Pages/Purchases/Create.vue
+++ resources/js/Pages/Purchases/Create.vue
@@ -1,18 +1,79 @@
 <script setup>
 import { getToday } from '@/common';
-import { onMounted, reactive } from 'vue';
+import { computed, onMounted, reactive, ref } from 'vue';
 
+const props = defineProps({
+    'customers': Array,
+    'items': Array,
+})
+
+const itemList = ref([])
 
 onMounted(() => {
     form.date = getToday()
+    props.items.forEach((item) => {
+        itemList.value.push({
+            id: item.id,
+            name: item.name,
+            price: item.price,
+            quantity: 0,
+        })
+    })
 })
+
+const quantity = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
 
 const form = reactive({
     date: null,
+    customer_id: null,
+})
+
+const totalPrice = computed(() => {
+    let total = 0
+    itemList.value.forEach((item) => {
+        total += item.price * item.quantity
+    })
+
+    return total
 })
 </script>
 
 <template>
     日付 <br>
-    <input type="date" name="date" v-model="form.date">
+    <input type="date" name="date" v-model="form.date"><br>
+    会員名<br>
+    <select name="customer" v-model="form.customer_id">
+        <option v-for="customer in customers" :value="customer.id" :key="customer.id">
+            {{ customer.id }} : {{ customer.name }}
+        </option>
+    </select>
+    <br><br>
+
+    商品・サービス<br>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>商品名</th>
+                <th>金額</th>
+                <th>数量</th>
+                <th>小計</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in itemList" :key="item.id">
+                <td>{{ item.id }}</td>
+                <td>{{ item.name }}</td>
+                <td>{{ item.price }}</td>
+                <td>
+                    <select name="quantity" v-model="item.quantity">
+                        <option v-for="q in quantity" :value="q">{{ q }}</option>
+                    </select>
+                </td>
+                <td>{{ item.price * item.quantity }}</td>
+            </tr>
+        </tbody>
+    </table>
+    <br>
+    合計 {{ totalPrice }}
 </template>
