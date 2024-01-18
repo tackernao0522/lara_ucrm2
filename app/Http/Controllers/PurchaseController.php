84ca4cad56fdd75fe63374def740206bb43e426b
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2024-01-18 14:19:21 +0900

    78. 保存処理その3 トランザクション追加

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -7,6 +7,7 @@ use App\Models\Customer;
 use App\Models\Item;
 use App\Models\Purchase;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class PurchaseController extends Controller
@@ -46,19 +47,27 @@ class PurchaseController extends Controller
      */
     public function store(StorePurchaseRequest $request)
     {
-        $purchase = Purchase::create([
-            'customer_id' => $request->customer_id,
-            'status' => $request->status,
-        ]);
+        DB::beginTransaction();
 
-        foreach ($request->items as $item) {
-            $purchase->items()->attach($purchase->id, [
-                'item_id' => $item['id'],
-                'quantity' => $item['quantity'],
+        try {
+            $purchase = Purchase::create([
+                'customer_id' => $request->customer_id,
+                'status' => $request->status,
             ]);
-        }
 
-        return to_route('dashboard');
+            foreach ($request->items as $item) {
+                $purchase->items()->attach($purchase->id, [
+                    'item_id' => $item['id'],
+                    'quantity' => $item['quantity'],
+                ]);
+            }
+
+            DB::commit();
+
+            return to_route('dashboard');
+        } catch (\Exception $e) {
+            DB::rollBack();
+        }
     }
 
     /**
