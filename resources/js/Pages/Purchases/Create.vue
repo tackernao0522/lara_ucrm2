71ed6163b3053c8c04b35ff0951ac410e6053ad5
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2024-01-21 14:29:46 +0900

    79. Purchase/Create.vueのレイアウト調整

--- resources/js/Pages/Purchases/Create.vue
+++ resources/js/Pages/Purchases/Create.vue
@@ -1,9 +1,12 @@
 <script setup>
-import { getToday } from '@/common';
-import { router } from '@inertiajs/vue3';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import BreezeValidationErrors from '@/Components/ValidationErrors.vue'
+import { Head, router } from '@inertiajs/vue3';
 import { computed, onMounted, reactive, ref } from 'vue';
+import { getToday } from '@/common';
 
 const props = defineProps({
+    'errors': Object,
     'customers': Array,
     'items': Array,
 })
@@ -55,44 +58,104 @@ const storePurchase = () => {
 </script>
 
 <template>
-    <form @submit.prevent="storePurchase">
-        日付 <br>
-        <input type="date" name="date" v-model="form.date"><br>
-        会員名<br>
-        <select name="customer" v-model="form.customer_id">
-            <option v-for="customer in customers" :value="customer.id" :key="customer.id">
-                {{ customer.id }} : {{ customer.name }}
-            </option>
-        </select>
-        <br><br>
+    <Head title="購入画面" />
 
-        商品・サービス<br>
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>商品名</th>
-                    <th>金額</th>
-                    <th>数量</th>
-                    <th>小計</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="item in itemList" :key="item.id">
-                    <td>{{ item.id }}</td>
-                    <td>{{ item.name }}</td>
-                    <td>{{ item.price }}</td>
-                    <td>
-                        <select name="quantity" v-model="item.quantity">
-                            <option v-for="q in quantity" :value="q" :key="q">{{ q }}</option>
-                        </select>
-                    </td>
-                    <td>{{ item.price * item.quantity }}</td>
-                </tr>
-            </tbody>
-        </table>
-        <br>
-        合計 {{ totalPrice }} <br><br>
-        <button>登録する</button>
-    </form>
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">購入画面</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <BreezeValidationErrors class="mb-4 text-center" :errors="errors" />
+                        <section class="text-gray-600 body-font relative">
+                            <form @submit.prevent="storePurchase">
+                                <div class="container px-5 py-8 mx-auto">
+                                    <div class="lg:w-1/2 md:w-2/3 mx-auto">
+                                        <div class="flex flex-wrap -m-2">
+                                            <div class="p-2 w-full">
+                                                <div class="relative">
+                                                    <label for="date" class="leading-7 text-sm text-gray-600">日付</label>
+                                                    <input type="date" id="date" name="date" v-model="form.date"
+                                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">
+                                                </div>
+                                            </div>
+                                            <div class="p-2 w-full">
+                                                <div class="relative">
+                                                    <label for="customer"
+                                                        class="leading-7 text-sm text-gray-600">会員名</label>
+                                                    <select id="customer" name="customer" v-model="form.customer_id"
+                                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">
+                                                        <option v-for="customer in customers" :value="customer.id"
+                                                            :key="customer.id">
+                                                            {{ customer.id }} : {{ customer.name }}
+                                                        </option>
+                                                    </select>
+                                                </div>
+                                            </div>
+
+                                            <div class="w-full mt-8 mx-auto overflow-auto">
+                                                <table class="table-auto w-full text-left whitespace-nowrap">
+                                                    <thead>
+                                                        <tr>
+                                                            <th
+                                                                class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
+                                                                ID</th>
+                                                            <th
+                                                                class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                                                商品名</th>
+                                                            <th
+                                                                class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                                                金額</th>
+                                                            <th
+                                                                class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                                                数量</th>
+                                                            <th
+                                                                class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                                                小計</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr v-for="item in itemList" :key="item.id">
+                                                            <td class="border-b-2 px-4 py-3">{{ item.id }}</td>
+                                                            <td class="border-b-2 px-4 py-3">{{ item.name }}</td>
+                                                            <td class="border-b-2 px-4 py-3">{{ item.price }}</td>
+                                                            <td class="border-b-2 px-4 py-3">
+                                                                <select name="quantity" v-model="item.quantity">
+                                                                    <option v-for="q in quantity" :value="q" :key="q">{{ q
+                                                                    }}</option>
+                                                                </select>
+                                                            </td>
+                                                            <td class="border-b-2 px-4 py-3">
+                                                                {{ item.price * item.quantity }}
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+
+                                            <div class="p-2 w-full">
+                                                <div class="relative">
+                                                    <label for="price" class="leading-7 text-sm text-gray-600">合計金額</label>
+                                                    <div id="price"
+                                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">
+                                                        {{ totalPrice }} 円</div>
+                                                </div>
+                                            </div>
+                                            <div class="p-2 w-full">
+                                                <button
+                                                    class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録する</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
 </template>
