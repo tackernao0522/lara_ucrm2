185b1b8813e4527e27e24647b4b2c651042897ae
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2024-02-03 14:24:24 +0900

    101. 日別売上のクエリ作成・取得確認

--- app/Http/Controllers/Api/AnalysisController.php
+++ app/Http/Controllers/Api/AnalysisController.php
@@ -3,16 +3,30 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 
 class AnalysisController extends Controller
 {
     public function index(Request $request)
     {
+        $subQuery = Order::betweenDate($request->startDate, $request->endDate);
+
+        if ($request->type === 'perDay') {
+            $subQuery->where('status', true)->groupBy('id')
+                ->selectRaw('id, SUM(subtotal) as totalPerPurchase, DATE_FORMAT(created_at, "%Y%m%d") as date')
+                ->groupBy('date');
+
+            $data = DB::table($subQuery)
+                ->groupBy('date')
+                ->selectRaw('date, SUM(totalPerPurchase) as total')->get();
+        }
         // Ajax通信なのでJson形式で返却する必要がある
         return response()->json([
-            'data' => $request->startDate // 仮設定
+            'data' => $data, // 仮設定
+            'type' => $request->type,
         ], Response::HTTP_OK);
     }
 }
