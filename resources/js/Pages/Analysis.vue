bf1092e4ad430f44919fd6f5ea3d632f0409e310
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2024-02-03 13:51:12 +0900

    97. Analisisページ追加 98. 期間を指定する 99. Analiysis.vueにフォームを追加

---
+++ resources/js/Pages/Analysis.vue
@@ -0,0 +1,42 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { getToday } from '@/common';
+import { Head } from '@inertiajs/vue3';
+import { onMounted, reactive } from 'vue';
+
+onMounted(() => {
+    form.startDate = getToday()
+    form.endDate = getToday()
+})
+
+const form = reactive({
+    startDate: null,
+    endDate: null,
+})
+</script>
+
+<template>
+    <Head title="データ分析" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">データ分析</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <form>
+                            From: <input type="date" name="startDate" v-model="form.startDate">
+                            To: <input type="date" name="endDate" v-model="form.endDate">
+                            <br>
+                            <button
+                                class="mt-4 flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">分析する</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
