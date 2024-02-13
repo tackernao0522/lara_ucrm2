d4b4e6366e465ef6fae9230f041ebd8d519ece9d
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2024-02-13 20:20:58 +0900

    109. デシル分析1 会員毎にまとめて金額順にソートする

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -12,26 +12,22 @@ class AnalysisController extends Controller
     public function index()
     {
         // 期間指定
-        // $startDate = '2022-08-01';
-        // $endDate = '2022-8-31';
+        $startDate = '2022-08-01';
+        $endDate = '2022-8-31';
 
-        // $period = Order::betweenDate($startDate, $endDate)
-        //     ->groupBy('id')
-        //     ->selectRaw('id, sum(subtotal) as total,
-        // customer_name, status, created_at')
-        //     ->orderBy('created_at')
-        //     ->paginate(50);
+        // 1. 購買ID毎にまとめる
+        $subQuery = Order::betweenDate($startDate, $endDate)
+            ->groupBy('id')
+            ->selectRaw('id, customer_id, customer_name,
+        SUM(subtotal) as totalPerPurchase');
 
-        // dd($period);
+        // 2. 会員毎にまとめて購入金額順にソートする
+        $subQuery = DB::table($subQuery)
+            ->groupBy('customer_id')
+            ->selectRaw('customer_id, customer_name, SUM(totalPerPurchase) as total')
+            ->orderBy('total', 'desc')->get();
 
-        // 日別
-        // $subQuery = Order::betweenDate($startDate, $endDate)->where('status', true)->groupBy('id')
-        //     ->selectRaw('id, SUM(subtotal) as totalPerPurchase, DATE_FORMAT(created_at, "%Y%m%d") as date');
-        // $data = DB::table($subQuery)
-        //     ->groupBy('date')
-        //     ->selectRaw('date, sum(totalPerPurchase) as total')->get();
-
-        // dd($data);
+        dd($subQuery);
 
         return Inertia::render('Analysis');
     }
